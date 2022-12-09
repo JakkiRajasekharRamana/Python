9fb7b9f0b209bce1a5ba6903eb5efb439401a60a
Jakki_Rajasekhar_Ramana <114324888+JakkiRajasekharRamana@users.noreply.github.com>
2022-12-10 00:53:53 +0530

    Add files via upload

---
+++ Finding_If_The_Entered_Alphabet_Is_Vowel_Or_Consonet.py
@@ -0,0 +1,7 @@
+#4)
+x=input('Enter One Character:')
+if(x=='A' or x=='a' or x=='E'or x=='e' or x=='I' or x=='i' or x=='O' or x=='o' or x=='U' or x=='u'):
+    print('Vowel')
+else:
+    print('Consonant')
+
