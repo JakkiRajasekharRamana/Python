22ae389dfb6832edbdb73fa6798312e7a5aefa86
Jakki_Rajasekhar_Ramana <114324888+JakkiRajasekharRamana@users.noreply.github.com>
2022-12-20 22:48:56 +0530

    Add files via upload

---
+++ Asterisks_Graph_4.py.py
@@ -0,0 +1,9 @@
+n=int(input("Enter Value"))
+
+for i in range(n):
+    for j in range(n-(i+1)):
+        print(" ",end="")
+    for k in range(i+1):
+        print("*",end="")
+    print("\n")
+    
